231000f8f53f40858c7d2774b9bd437968e258e4
sakisalman <codersaki98@gmail.com>
2022-08-31 14:40:55 -0700

    add logout

--- social/src/Pages/Home/Home.jsx
+++ social/src/Pages/Home/Home.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import AuthContext from '../../Contexts/AuthContext'
+import Cookies from 'js-cookie'
 
 const Home = () => {
+
+
+  // auth context
+  const {dispatch} = useContext(AuthContext)
+
+  // handle Logout
+  const handleLogout = () => {
+    Cookies.remove('access_token')
+    dispatch({type : "LOGOUT"})
+  }
   return (
     <div>
         <div className="wrap d-flex justify-content-center">
           <div className="card">
             <div className="card-body">
               <h2>Under Constraction!</h2>
-              
+              <button className='btn btn-danger w-100' onClick={handleLogout}>Log Out</button>
+
             </div>
           </div>
         </div>
